36c92f9b7a65645ba30fd0fe1a284579c0375c86
Dinand <cassiel.danniel@gmail.copm>
2024-08-30 20:49:05 +0700

    Initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = '';
-//$config['base_url'] = 'http://localhost/Kuliah/Framework/CI-3/';
 
 /*
 |--------------------------------------------------------------------------
